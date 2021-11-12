0fc8ce6029c758d4f5d0ff101ff1382024eeb56d
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-11-12 09:44:51 +0530

    Delete Print_10_To_1.java

--- Print_10_To_1.java
+++
@@ -1,9 +0,0 @@
-public class Print_10_To_1 {
-    public static void main(String args[]){
-        int i=10;
-        while(i>=1){
-            System.out.println(i);
-            i--;
-        }
-    }
-}
