c6f2583509aea4e85f9054a31ea6f12b5820aea7
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-11-12 08:54:20 +0530

    Add files via upload

---
+++ Patter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Patter {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n=sc.nextInt();
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0;j<=i;j++)
+              System.out.print("* ");
+              System.out.println();
+        }
+    }
+}
